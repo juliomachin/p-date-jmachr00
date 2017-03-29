deab9569e9a199b444ed17a7a200872116b65b2b
juliomachin <jmachr00@estudiantes.unileon.es>
2017-03-29 19:23:45 +0200

    Modificados clase Date

--- Date.java
+++ Date.java
@@ -5,43 +5,79 @@ public class Date {
 	private int month;
 	private int year;
 
-	public int getday(){
+	public int getDay(){
 		return day;	
 	}
 	
-	public void setday(int day) throws date_Exception{
+	public void setDay(int day) throws Date_Exception{
 		if(day <=0 ){
-			throw new date_Exception("El número de dias no puede ser menor que 0" +month+ "\n");
-		}else if (day >31){
-			throw new date_Exception("El número de los Meses no puede ser mayor que 12" +month+ "\n");
+			throw new Date_Exception("El número de dias no puede ser menor que 0" +month+ "\n");
+		}else if (!this.isDayRigth(day)){
+			throw new Date_Exception("El número del Mese no es correcto " +month+ "\n");
 		}else{
 			this.month = month;
 		}
 	}
 	
-	public int getmonth(){
+	public int getMonth(){
 		return month;
 	}
 	
-	public void setmont(int month) throws date_Exception{
+	public void setMonth(int month) throws Date_Exception{
 		if(month <=0 ){
-			throw new date_Exception("El número de los Meses no puede ser menor que 0" +month+ "\n");
+			throw new Date_Exception("El número de los Meses no puede ser menor que 0" +month+ "\n");
 		}else if (month >12){
-			throw new date_Exception("El número de los Meses no puede ser mayor que 12" +month+ "\n");
+			throw new Date_Exception("El número de los Meses no puede ser mayor que 12" +month+ "\n");
 		}else{
 			this.month = month;
 		}
 	}
 
-	/*public Date (int day , int month, int year){
-			this.day = day;
-			this.month = month;
-			this.year = year;
-			today = new Date("Fecha");
+	public int getYear(int year) throws dateException{
+		if(year < 0){
+			throw new Date_Exception("Un año no puede ser menor que cero" +year+ "\n");
+		}else if (year)
+		return year;
+	}
 	
-			Date(int day, int month, int year){
-				if(day ==)
-			}
-	}*/
+	public void setYear()
+
+	public int dayOfMonth(){
+		return this.dayOfMonth (this.month);
+	}
+
+	private int dayOfMonth(int month){
+		int m = 0;
+		switch(month){
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:	
+			case 12:
+				m = 31;
+				break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				m = 30;
+				break;
+			case 2:
+				m = 28;
+				break;
+			default:
+				m = -1;
+			break;	
+		}
+		return m;	
+	}
+		
+	private boolean isDayRight(int day){
+		return ((day >0) && (day <= this.dayOfMonth(this.month)));
+	}
+	
+
 		
 }
