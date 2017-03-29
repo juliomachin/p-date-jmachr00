0fdda5ff5b938557773aac43814e062c0f0595b8
Pablo Machin <Pablo Machin@192.168.1.35>
2017-03-30 00:17:18 +0200

    Añadido Constructot

--- Date.java
+++ Date.java
@@ -4,16 +4,55 @@ public class Date {
 	private int day;
 	private int month;
 	private int year;
+	
+	//Crear Constructor
+	public Date(){
+		this.day = 0;
+		this.month = 0;
+		this.year = 0;
+	}
+	
+	public Date(int dya, int month, int year) throws Date_Exception{
+		StringBuffer mensaje = new StringBuffer();
+		
+		if(day <= 0){
+			mensaje.append("No se permiten dias negativos" +day+ "\n");
+		}
+		if(month <=0){
+			mensaje.append("No se permiten meses negativos" +month+ "\n");
+		}else if(month > 12){
+			mensaje.append("No se permiten meses mayores que 12" +month+ "\n");
+		}
+		if (year < 0){
+			mensaje.append("No se permiten anyos negativos" +year+ "\n");
+		}
+		if (mensaje.length() != 0){
+			throw new Date_Exception(mensaje.toString());
+		}else{
+			this.day = day;
+			this.month = month;
+			this.year = year;
+		}
+	}
+	
+	public Date(Date another){
+		this.day = another.getDay();
+		this.month = another.getMonth();
+		this.year = another.getYear();
+	}
 
+	
+	//Getter and Setter
+	
 	public int getDay(){
 		return day;	
 	}
 	
 	public void setDay(int day) throws Date_Exception{
 		if(day <=0 ){
-			throw new Date_Exception("El número de dias no puede ser menor que 0" +month+ "\n");
+			throw new Date_Exception("El nummero de dias no puede ser menor que 0" +month+ "\n");
 		}else if (!this.isDayRigth(day)){
-			throw new Date_Exception("El número del Mese no es correcto " +month+ "\n");
+			throw new Date_Exception("El numero del Mese no es correcto " +month+ "\n");
 		}else{
 			this.month = month;
 		}
@@ -25,23 +64,28 @@ public class Date {
 	
 	public void setMonth(int month) throws Date_Exception{
 		if(month <=0 ){
-			throw new Date_Exception("El número de los Meses no puede ser menor que 0" +month+ "\n");
+			throw new Date_Exception("El numero de los Meses no puede ser menor que 0" +month+ "\n");
 		}else if (month >12){
-			throw new Date_Exception("El número de los Meses no puede ser mayor que 12" +month+ "\n");
+			throw new Date_Exception("El numero de los Meses no puede ser mayor que 12" +month+ "\n");
 		}else{
 			this.month = month;
 		}
 	}
 
-	public int getYear(int year) throws dateException{
-		if(year < 0){
-			throw new Date_Exception("Un año no puede ser menor que cero" +year+ "\n");
-		}else if (year)
+	public int getYear(int year) throws Date_Exception{
 		return year;
 	}
 	
-	public void setYear()
+	public void setYear(int year) throws Date_Exception{
+		if(year < 0){
+			throw new Date_Exception("El anyo no puede ser menor que cero" +year+ "\n");
+		}else{
+			this.year = year;
+		}
+	}
 
+	//Sacar que dia pertenece a cada mes
+	
 	public int dayOfMonth(){
 		return this.dayOfMonth (this.month);
 	}
